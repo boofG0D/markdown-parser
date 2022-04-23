14de470106f3ee8233747591d372c9ee436d9bd3
Michael Diaz <103291795+boofG0D@users.noreply.github.com>
2022-04-22 17:07:43 -0700

    Update MarkdownParseTest.java
    all test files pass in junit testing

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,5 +1,8 @@
 import static org.junit.Assert.*;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
 
 import org.junit.*;
@@ -11,8 +14,26 @@ public class MarkdownParseTest {
     } 
 
     @Test
-    public void LinksTest() {
-        
-        assertEquals(, List.of("https://something.com", "some-page.html"));
+    public void LinksTest() throws IOException{
+        String fileContent = Files.readString(Path.of("/Users/michaeldiaz/Documents/GitHub/markdown-parser/test-file.md"));
+        assertEquals(MarkdownParse.getLinks(fileContent), List.of("https://something.com", "some-thing.html"));
+    }
+
+    @Test
+    public void Links2Test() throws IOException{
+        String fileContent = Files.readString(Path.of("/Users/michaeldiaz/Documents/GitHub/markdown-parser/2test.md"));
+        assertEquals(MarkdownParse.getLinks(fileContent), List.of("https://google.com", "bruh.com"));
+    }
+
+    @Test
+    public void Links3Test() throws IOException{
+        String fileContent = Files.readString(Path.of("/Users/michaeldiaz/Documents/GitHub/markdown-parser/3test.md"));
+        assertEquals(MarkdownParse.getLinks(fileContent), List.of("youtube.com"));
+    }
+
+    @Test
+    public void Links4Test() throws IOException{
+        String fileContent = Files.readString(Path.of("/Users/michaeldiaz/Documents/GitHub/markdown-parser/4test.md"));
+        assertEquals(MarkdownParse.getLinks(fileContent), List.of("bruhmoment.com"));
     }
 }
